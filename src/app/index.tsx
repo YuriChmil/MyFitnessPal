57b57bb99bc33e578995fdef93e8676a538460c8
Yuriy <Yuriy@Owners-MacBook-Pro.local>
2024-03-17 16:45:10 +0200

    FoodListItem

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
+import FoodListItem from '../components/FoodListItem';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! Expo</Text>
+    {/*Food item View */}
+    <FoodListItem item={{label:'Pizza',cal:75, brand:'Dominos'}}/>
+    <FoodListItem item={{label:'Pizza',cal:50, brand:'New York'}}/>
+    
     </View>
   );
 }
-
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding:10,
+    gap:5,
   },
 });
